fa17901ed9a21122bc6f0586c62b9b0e1c78189c
Michael Fruen <mfruen@gmail.com>
2017-01-02 20:42:59 +0000

    Moved database structure into database.sql

--- database.py
+++ database.py
@@ -23,84 +23,13 @@ class Database:
             return int(cursor.fetchone()[0]) > 0
 
     def build_database(self):
+        query = ''
+
+        with open('database.sql', 'r') as file:
+            query = file.read()
+
         with closing(self.database.cursor()) as cursor:
-            cursor.execute("""
-                CREATE TABLE
-                    stream_alerts
-                (
-                    id INTEGER PRIMARY KEY ASC AUTOINCREMENT,
-                    username TEXT NOT NULL,
-                    alert_channel_did TEXT NOT NULL,
-                    alert_format TEXT NOT NULL
-                )
-            """)
-            cursor.execute("""
-                CREATE INDEX
-                    stream_alerts_username
-                ON
-                    stream_alerts
-                    (
-                        username
-                    )
-            """)
-
-            cursor.execute("""
-                CREATE TABLE
-                    stream_alert_messages
-                (
-                    id INTEGER PRIMARY KEY ASC AUTOINCREMENT,
-                    stream_alert_id INTEGER NOT NULL,
-                    channel_did TEXT NOT NULL,
-                    message_did TEXT NOT NULL
-                )
-            """)
-            cursor.execute("""
-                CREATE INDEX
-                    stream_alert_messages_stream_alert_id
-                ON
-                    stream_alert_messages
-                    (
-                        stream_alert_id
-                    )
-            """)
-
-            cursor.execute("""
-                CREATE TABLE
-                    admins
-                (
-                    id INTEGER PRIMARY KEY ASC AUTOINCREMENT,
-                    user_did TEXT NOT NULL,
-                    friendly_name TEXT NOT NULL
-                )
-            """)
-            cursor.execute("""
-                CREATE INDEX
-                    admins_user_did
-                ON
-                    admins
-                    (
-                        user_did
-                    )
-            """)
-
-            cursor.execute("""
-                CREATE TABLE
-                    command_aliases
-                (
-                    id INTEGER PRIMARY KEY ASC AUTOINCREMENT,
-                    command TEXT NOT NULL,
-                    alias TEXT NOT NULL
-                )
-            """)
-            cursor.execute("""
-                CREATE INDEX
-                    command_aliases_alias
-                ON
-                    command_aliases
-                    (
-                        alias
-                    )
-            """)
+            cursor.executescript(query)
 
         self.database.commit()
 
