f1500ff08f7c03c177bd3796b14409b6f5bcba65
Michael Fruen <mfruen@gmail.com>
2017-01-21 15:10:41 +0000

    refactored bot_commands.py

--- modules/commands/handlers/bot_commands.py
+++ modules/commands/handlers/bot_commands.py
@@ -10,63 +10,83 @@ class BotCommands:
         self.register(commands)
 
     def register(self, commands):
-        commands.register_handler(self._list_channels, 'list channels')
-        commands.register_handler(self._list_users, 'list users')
+        commands.register_handler(self.cmd_list_channels, 'list channels')
+        commands.register_handler(self.cmd_list_users, 'list users')
 
-    async def _list_channels(self, list_filter, message):
+    async def cmd_list_channels(self, attributes, message):
         channels = defaultdict(list)
-        return_text = '.\n'
 
+        for server, channel in self.get_text_channels(attributes):
+            channels[server].append(channel)
+
+        await self.bot.send_message(
+            message.channel,
+            self.get_channels_text(channels)
+        )
+
+    def get_text_channels(self, channel_filter):
         for server in self.bot.servers:
             for channel in server.channels:
-                if channel.type == discord.ChannelType.text:
-                    if list_filter.lower() in channel.name.lower():
-                        channels[server].append(channel)
+                if channel.type != discord.ChannelType.text:
+                    continue
 
+                if channel_filter.lower() not in channel.name.lower():
+                    continue
+
+                yield server, channel
+
+    def get_channels_text(self, channels):
         if not channels:
-            return await self.bot.send_message(
-                message.channel,
-                'No channels found'
-            )
+            return 'No channels found'
 
+        return '.\n{}'.format('\n'.join(self.get_channels_text_pieces(channels)))
+
+    def get_channels_text_pieces(self, channels):
         for server in channels.keys():
-            return_text += 'Server: `{}`\n'.format(server)
+            yield 'Server: `{}`'.format(server)
             for channel in channels[server]:
-                return_text += '    `{0.id}`: `{0.name}`'.format(channel)
+                channel_text = '    `{0.id}`: `{0.name}`'.format(channel)
 
                 if not channel.permissions_for(server.me).send_messages:
-                    return_text += ' (cannot message)'
+                    channel_text += ' (cannot message)'
 
-                return_text += '\n'
+                yield channel_text
 
-        await self.bot.send_message(message.channel, return_text)
+    async def cmd_list_users(self, attributes, message):
+        members = defaultdict(list)
 
-    async def _list_users(self, list_filter, message):
-        users = defaultdict(list)
-        return_text = '.\n'
+        for server, member in self.get_members(attributes):
+            members[server].append(member)
 
+        await self.bot.send_message(
+            message.channel,
+            self.get_users_text(members)
+        )
+
+    def get_members(self, user_filter):
         for server in self.bot.servers:
             for member in server.members:
-                if list_filter.lower() in member.name.lower():
-                    users[server].append(member)
+                if user_filter.lower() not in member.name.lower():
+                    continue
 
-        if not users:
-            return await self.bot.send_message(
-                message.channel,
-                'No users found'
-            )
+                yield server, member
 
-        for server in users.keys():
-            return_text += 'Server: `{}`\n'.format(server)
-            for member in users[server]:
-                return_text += '    `{0.id}`: `{0.name}`'.format(member)
+    def get_users_text(self, members):
+        if not members:
+            return 'No users found'
+
+        return '.\n{}'.format('\n'.join(self.get_users_text_pieces(members)))
+
+    def get_users_text_pieces(self, members):
+        for server in members.keys():
+            yield 'Server: `{}`'.format(server)
+            for member in members[server]:
+                member_text = '    `{0.id}`: `{0.name}`'.format(member)
 
                 if member.nick:
-                    return_text += ' `({0.nick})`'.format(member)
+                    member_text += ' `({0.nick})`'.format(member)
 
                 if member.bot:
-                    return_text += ' `BOT`'
+                    member_text += ' `BOT`'
 
-                return_text += '\n'
-
-        await self.bot.send_message(message.channel, return_text)
+                yield member_text
