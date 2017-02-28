baee0ab38d18d112bbdf98c5f1d6244f4f847c20
Michael Fruen <mfruen@gmail.com>
2017-02-28 02:35:01 +0000

    Limit levbot.py to LevBot specific logic
    Moved other logic into main.py

--- levbot.py
+++ levbot.py
@@ -1,17 +1,12 @@
-import os
-import logging
 import asyncio
-import discord
 import modules
-import settings
 
-from datetime import datetime
 from collections import defaultdict
 from itertools import chain
-from pushbulletlogging import PushbulletHandler
+from discord import Client
 
 
-class LevBot(discord.Client):
+class LevBot(Client):
     def __init__(self):
         super().__init__()
 
@@ -65,62 +60,3 @@ class LevBot(discord.Client):
             piece = piece.rsplit(' ', 1)[0]
 
         return piece, string[len(piece):]
-
-
-def set_up_logging():
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
-    formatter = logging.Formatter(
-        fmt='[%(asctime)s] %(levelname)s: %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S'
-    )
-
-    add_file_logger(logger, formatter)
-    add_console_logger(logger, formatter)
-    add_pushbullet_logger(logger, formatter)
-
-def add_file_logger(logger, formatter):
-    os.makedirs('logs', exist_ok=True)
-    filename = 'logs/{}.log'.format(
-        datetime.now().strftime('%Y-%m-%d %H-%M-%S')
-    )
-    filehandler = logging.FileHandler(filename)
-    filehandler.setLevel(logging.INFO)
-    filehandler.setFormatter(formatter)
-    logger.addHandler(filehandler)
-
-    return filehandler
-
-def add_console_logger(logger, formatter):
-    streamhandler = logging.StreamHandler()
-    streamhandler.setLevel(logging.ERROR)
-    streamhandler.setFormatter(formatter)
-    logger.addHandler(streamhandler)
-
-    return streamhandler
-
-def add_pushbullet_logger(logger, formatter):
-    if not settings.pushbullet_token:
-        return None
-
-    pushbullethandler = PushbulletHandler(
-        access_token=settings.pushbullet_token
-    )
-    pushbullethandler.setLevel(logging.ERROR)
-    pushbullethandler.setFormatter(formatter)
-    logger.addHandler(pushbullethandler)
-
-    return pushbullethandler
-
-def remove_pushbullet_logger():
-    logger = logging.getLogger()
-    logger.handlers = [h for h in logger.handlers if not isinstance(h, PushbulletHandler)]
-
-
-if __name__ == '__main__':
-    set_up_logging()
-
-    bot = LevBot()
-    bot.run(settings.bot_token)
-
-    remove_pushbullet_logger()
